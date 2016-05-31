c577e74b6b5fe54af3a71e09e54ffcb8520093f7
bmoby <msouleymanov@gmail.com>
2016-05-31 02:18:29 +0200

    ocherednoi test 6

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+    ...
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+    ...
+end
