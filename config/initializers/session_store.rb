8cb6e431bbf9c01f946c74dfaa6c621f7e7012c0
bmoby <msouleymanov@gmail.com>
2016-05-31 03:56:29 +0200

    renommer de nouveau lappli

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_marshruti_session'
+Rails.application.config.session_store :cookie_store, key: '_mymarshrut_session'
