5eb67205c877df2274a1103e59f1a20d07829dd1
bmoby <msouleymanov@gmail.com>
2016-06-06 17:29:01 +0200

    configuration dragonfly to s3

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,4 +1,5 @@
 require 'dragonfly'
+require 'dragonfly/s3_data_store'
 
 # Configure
 Dragonfly.app.configure do
@@ -7,6 +8,17 @@ Dragonfly.app.configure do
   secret "6ad74dec6da4d687f229ec062a3e16f96563b25c19907bb3b146cbca7851d12c"
 
   url_format "/media/:job/:name"
+  if Rails.env.development? || Rails.env.test?
+    datastore :file,
+              root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+              server_root: Rails.root.join('public')
+  else
+    datastore :s3,
+              bucket_name: mymarshrutimages,
+              access_key_id: AKIAIGJ3FKUDUF6TCLZQ,
+              secret_access_key: 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,
+              url_scheme: 'https'
+  end
 
   datastore :file,
     root_path: Rails.root.join('public/system/dragonfly', Rails.env),
