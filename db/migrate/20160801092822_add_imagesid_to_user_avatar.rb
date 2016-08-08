0de4ab2d4c868f67ef3a0e1d9eb6c93f7413da78
DrMerjo <ma_darvin@hotmail.fr>
2016-08-08 12:25:07 +0200

    create listing md

--- db/migrate/20160801092822_add_imagesid_to_user_avatar.rb
+++
@@ -1,6 +0,0 @@
-class AddImagesidToUserAvatar < ActiveRecord::Migration
-  def change
-  	add_column :user_avatars, :image_uid, :string
-  	add_column :user_avatars, :image_name, :string
-  end
-end
--- db/migrate/20160801124821_destroy_table_user_avatar.rb
+++
@@ -1,5 +0,0 @@
-class DestroyTableUserAvatar < ActiveRecord::Migration
-  def change
-  	drop_table :user_avatars
-  end
-end
